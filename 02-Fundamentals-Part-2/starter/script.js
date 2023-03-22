183ad6d860281e1e681153f0cebbcfb45d4b4dde
Max Loncar <max.loncar@devot.team>
2023-03-22 16:03:04 +0100

    Looping backwards and Loops in Loops - script.js

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -430,3 +430,28 @@ for (let i = 0; i < max.length; i++) {
   console.log(max[i], typeof max[i]);
 }
 */
+
+// LOOPING BACKWARDS AND LOOPS IN LOOPS
+const max = [
+  'Max',
+  'Lončar',
+  2023 - 1998,
+  'developer',
+  ['Kristijan', 'Stjepan', 'Tomislav'],
+  true
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for (let i = max.length - 1; i >= 0; i--) {
+  console.log(i, max[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
