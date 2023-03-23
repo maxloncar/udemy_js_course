b89fad7598b1356f09b29206d15a2e828a39e940
Max Loncar <max.loncar@devot.team>
2023-03-23 10:58:35 +0100

    Section 3: Coding Challenge #4

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -476,3 +476,46 @@ while (dice !== 6) {
   if (dice === 6) console.log('Loop is about to end...');
 }
 */
+
+// CODING CHALLENGE #4
+const bills = [
+  22,
+  295,
+  176,
+  440,
+  37,
+  105,
+  10,
+  1100,
+  86,
+  52
+];
+
+const tips = [], totals = [];
+
+const calcTip = bill => {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+for (let i = 0; i < bills.length; i++) {
+  // tips.push(calcTip(bills[i]));
+  // totals.push(bills[i] + calcTip(bills[i]));
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+}
+
+console.log(bills, tips, totals);
+
+// Bonus
+const calcAverage = (arr) => {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+};
+
+console.log(calcAverage(totals));
