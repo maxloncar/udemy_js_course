c1a394c187dea1bdd9a535c7f43cfdc968e20ea0
Max Loncar <max.loncar@devot.team>
2023-03-30 12:34:56 +0200

    Section 8: Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+// SCOPING IN PRACTICE
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
 
@@ -35,4 +37,48 @@ function calcAge(birthYear) {
 const firstName = 'Max';
 calcAge(1996);
 // console.log(age);
-// 3printAge();
+// printAge();
+*/
+
+// HOISTING AND TDZ IN PRACTICE
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Max';
+let job = 'developer';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
