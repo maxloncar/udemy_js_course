7043430b8d27f22dc012416c628410e4abe9faba
Max Loncar <max.loncar@devot.team>
2023-03-31 17:02:22 +0200

    Section 9: Coding Challenge #1 -> in progress

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,6 +54,79 @@ const restaurant = {
   },
 };
 
+// CODING CHALLENGE #1
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnabry',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnabry', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1)
+const [players1, players2] = game.players;
+
+// 2)
+const [gk, ...fieldPlayers] = players1;
+
+// 3)
+const allPlayers = [...players1, ...players2];
+
+// 4)
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// 5)
+const { team1, x: draw, team2 } = game.odds;
+
+// 6)
+const printGoals = function (...playerNames) {
+  console.log(...playerNames);
+  console.log(`There were scored ${playerNames.length} goals!`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7)
+console.log(
+  (team1 < team2 && `${game.team1} is more likely to win!`) ||
+    `${game.team2} is more likely to win!`
+);
+
+/*
 // LOGICAL ASSIGNMENT OPERATORS
 const rest1 = {
   name: 'Capri',
@@ -85,7 +158,7 @@ rest2.owner &&= '<ANONYMOUS>';
 console.log(rest1);
 console.log(rest2);
 
-/*
+
 // THE NULLISH COALESCING OPERATOR (??)
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
