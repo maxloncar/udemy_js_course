09d991630aa05f0875a5f96d783cd5782d679764
Max Loncar <max.loncar@devot.team>
2023-04-03 10:57:54 +0200

    Section 9: Optional Chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,7 +54,32 @@ const restaurant = {
   },
 };
 
-// ENHANCED OBJECT LITERALS
+// OPTIONAL CHAINING (?.)
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// With optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [{ name: 'Max', email: 'max.loncar@devot.team' }];
+console.log(users[0]?.name ?? 'User array empty');
+// console.log(users[0]?.lastName ?? 'User array empty');
+// OR
+if (users.length > 0) console.log(users[0].name);
+else console.log('User array empty');
 
 /*
 // LOOPING ARRAYS: THE FOR-OF LOOP
