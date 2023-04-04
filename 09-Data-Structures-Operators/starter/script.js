f39a51f00b973c4e59b0d276181cbe60eaf95366
Max Loncar <max.loncar@devot.team>
2023-04-04 15:50:26 +0200

    Section 9: Working With Strings - Part 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -109,6 +109,59 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
+// WORKING WITH STRINGS - PART 3
+// Split and join
+console.log('a+very+nice+string'.split('+'));
+console.log('Max Lončar'.split(' '));
+
+const [firstName, lastName] = 'Max Lončar'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('max lončar');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(25, '+').padEnd(30, '+'));
+console.log('Max'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + ''; // converting to string
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(49184901859071551));
+console.log(maskCreditCard(43339184));
+console.log(maskCreditCard('9087508971057019750891950'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'🛩️}'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
 // WORKING WITH STRINGS - PART 2
 const airline = 'TAP Air Croatia';
 
@@ -183,7 +236,7 @@ checkBaggage('Ihave a laptop, some Food and a pocket Knife');
 checkBaggage('Socks and camera');
 checkBaggage('Got some snacks and a gun for protection');
 
-/*
+
 // WORKING WITH STRINGS - PART 1
 const airline = 'TAP Air Croatia';
 const plane = 'A320';
