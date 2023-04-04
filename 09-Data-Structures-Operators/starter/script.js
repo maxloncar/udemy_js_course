c13c38fbf848c78a0e44f1d1f4e6449227de95cd
Max Loncar <max.loncar@devot.team>
2023-04-04 13:44:07 +0200

    Section 9: Coding Challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -95,6 +95,41 @@ const game = {
   },
 };
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// CODING CHALLENGE #3
+// 1)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2)
+gameEvents.delete(64);
+
+// 3)
+const time = [...gameEvents.keys()].pop(); // get last (92nd) minute
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4)
+for (const [key, value] of gameEvents) {
+  const half = key <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${key}: ${value}`);
+}
+
+/*
 // MAPS: ITERATION
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
@@ -132,7 +167,7 @@ console.log([...question]);
 console.log([...question.keys()]);
 console.log([...question.values()]);
 
-/*
+
 // MAPS: FUNDAMENTALS
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
