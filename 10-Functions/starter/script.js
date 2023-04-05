3c7c06b1e715d9ca83f4f7e2be68ce3da3398379
Max Loncar <max.loncar@devot.team>
2023-04-05 14:35:20 +0200

    Section 10: Functions Accepting Callback Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -29,7 +29,7 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
+
 
 // HOW PASSING ARGUMENTS WORKS: VALUE VS. REFERENCE
 const flight = 'LH324';
@@ -63,3 +63,33 @@ const newPassport = function (person) {
 
 newPassport(max);
 checkIn(flight, max);
+*/
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('✋');
+};
+
+document.body.addEventListener('click', high5);
+['Max', 'Marta', 'Filip'].forEach(high5);
