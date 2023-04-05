ea975f40079990a0366e60a78e89bf94463cdfdf
Max Loncar <max.loncar@devot.team>
2023-04-05 13:37:39 +0200

    Section 10: How Passing Arguments Works - Value vs. Reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // DEFAULT PARAMETERS
 const bookings = [];
 
@@ -28,3 +29,37 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+// HOW PASSING ARGUMENTS WORKS: VALUE VS. REFERENCE
+const flight = 'LH324';
+const max = {
+  name: 'Max Lončar',
+  passport: 82951897,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 82951897) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, max);
+// console.log(flight);
+// console.log(max);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = max;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+};
+
+newPassport(max);
+checkIn(flight, max);
