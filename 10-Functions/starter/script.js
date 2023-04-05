b6e34b2fdabed1993dde09991bd98cf659b17e14
Max Loncar <max.loncar@devot.team>
2023-04-05 15:39:37 +0200

    Section 10: The Call and Apply Methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -93,7 +93,7 @@ const high5 = function () {
 
 document.body.addEventListener('click', high5);
 ['Max', 'Marta', 'Filip'].forEach(high5);
-*/
+
 
 // FUNCTIONS RETURNING FUNCTIONS
 const greet = function (greeting) {
@@ -114,3 +114,55 @@ const greeterHello = greetArrow('Hello');
 greeterHello('Janko');
 
 greetArrow('Hi')('Djuro');
+*/
+
+// THE CALL AND APPLY METHODS
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function(){}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Max Lončar');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
