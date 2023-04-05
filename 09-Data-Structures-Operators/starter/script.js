365027f3eaaab678b4ad6ec76a5dd22a0f687c36
Max Loncar <max.loncar@devot.team>
2023-04-05 12:24:17 +0200

    Section 9: String Methods Practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
@@ -109,6 +105,26 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// STRING METHODS PRACTICE
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(45);
+  console.log(output);
+}
+
+/*
 // CODING CHALLENGE #4
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
@@ -138,7 +154,7 @@ document.querySelector('button').addEventListener('click', () => {
   }
 });
 
-/*
+
 // WORKING WITH STRINGS - PART 3
 // Split and join
 console.log('a+very+nice+string'.split('+'));
