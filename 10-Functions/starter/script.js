22a44a96aa1f44a03cd5417ed0954a40a3e3d58a
Max Loncar <max.loncar@devot.team>
2023-04-07 11:48:56 +0200

    Section 10: Immediately Invoked Function Expressions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -215,7 +215,7 @@ console.log(addVAT2(100));
 console.log(addVAT2(23));
 
 console.log(addTaxRate(0.15)(200));
-*/
+
 
 // CODING CHALLENGE #1
 const poll = {
@@ -263,3 +263,29 @@ poll.displayResults.call({ answers: [5, 2, 3] });
 poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+*/
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+const runOnce = function () {
+  console.log('This will never run again');
+};
+
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
