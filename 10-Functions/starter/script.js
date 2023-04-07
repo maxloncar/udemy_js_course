e1b7a599eccb09101d8a8c2063698e61c747a23a
Max Loncar <max.loncar@devot.team>
2023-04-07 11:20:19 +0200

    Section 10: Coding Challenge #1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -114,7 +114,7 @@ const greeterHello = greetArrow('Hello');
 greeterHello('Janko');
 
 greetArrow('Hi')('Djuro');
-*/
+
 
 // THE CALL AND APPLY METHODS
 const lufthansa = {
@@ -215,3 +215,51 @@ console.log(addVAT2(100));
 console.log(addVAT2(23));
 
 console.log(addTaxRate(0.15)(200));
+*/
+
+// CODING CHALLENGE #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    typeof answer === 'number' && answer >= 0 && answer < this.answers.length
+      ? this.answers[answer]++
+      : console.log('Wrong input!');
+
+    // OR: typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// OR
+// document.querySelector('.poll').addEventListener('click', () => {
+//   poll.registerNewAnswer();
+// });
+
+// Bonus
+poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
