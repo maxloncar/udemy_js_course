e0b4f02ee80aff6360363b4665b8fcb2bf4c4b1b
Max Loncar <max.loncar@devot.team>
2023-04-11 09:48:03 +0200

    Section 11: forEach With Maps and Sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -122,7 +116,7 @@ console.log(arr.at(-1));
 
 console.log('max'.at(0));
 console.log('max'.at(-1));
-*/
+
 
 // LOOPING ARRAYS: FOREACH
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -148,3 +142,24 @@ movements.forEach(function (movement, index, array) {
 // 1: function(450)
 // 2: function(-400)
 // ...
+*/
+
+// FOREACH WITH MAPS AND SETS
+// Map
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  // _ means throwable
+  console.log(`${value}: ${value}`);
+});
