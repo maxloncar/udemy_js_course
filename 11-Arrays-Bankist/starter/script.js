15481644e4b12108711457a27f187bc355b70050
Max Loncar <max.loncar@devot.team>
2023-04-11 15:25:58 +0200

    Section 11: The reduce Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -81,6 +81,13 @@ const displayMovements = movements => {
 };
 displayMovements(account1.movements);
 
+const calcDisplayBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcDisplayBalance(account1.movements);
+
 const createUsernames = accs => {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -91,7 +98,6 @@ const createUsernames = accs => {
   });
 };
 createUsernames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -254,7 +260,7 @@ const movementDescriptions = movements.map(
 );
 
 console.log(movementDescriptions);
-*/
+
 
 // THE FILTER METHOD
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
@@ -273,3 +279,27 @@ console.log(depositsFor);
 
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
+*/
+
+// THE REDUCE METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
