6199eae5dcf7c61fd856b12961fb7e3c262b5aaf
Max Loncar <max.loncar@devot.team>
2023-04-11 13:22:25 +0200

    Section 11: Coding Challenge #1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -185,4 +185,32 @@ currenciesUnique.forEach(function (value, _, map) {
 });
 */
 
-// CREATING DOM ELEMENTS
+// CODING CHALLENGE #1
+const checkDogs = (dogsJulia, dogsKate) => {
+  const dogsJuliaCopy = [...dogsJulia];
+  // OR
+  // const dogsJuliaCopy = dogsJulia.slice();
+
+  dogsJuliaCopy.splice(-2);
+  dogsJuliaCopy.splice(0, 1);
+  // OR
+  // dogsJulia.slice(1, 3);
+
+  const dogs = [...dogsJuliaCopy, ...dogsKate];
+  // OR
+  // const dogs - dogsJuliaCopy.concat(dogsKate);
+
+  dogs.forEach((dogAge, index) => {
+    if (dogAge >= 3) {
+      console.log(
+        `Dog number ${index + 1} is and adult, and is ${dogAge} years old`
+      );
+    } else {
+      console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+    }
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log('------------------------------');
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
