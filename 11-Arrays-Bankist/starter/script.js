ace788cfab81f932377c33bfcbf71e59842cbb50
Max Loncar <max.loncar@devot.team>
2023-04-11 09:42:21 +0200

    Section 11: Looping Arrays - forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -108,7 +108,7 @@ console.log([...arr, ...arr2]);
 
 // join
 console.log(letters.join(' - '));
-*/
+
 
 // THE NEW AT METHOD
 const arr = [23, 11, 64];
@@ -122,3 +122,29 @@ console.log(arr.at(-1));
 
 console.log('max'.at(0));
 console.log('max'.at(-1));
+*/
+
+// LOOPING ARRAYS: FOREACH
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('-----FOREACH------');
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+// 0: function(200)
+// 1: function(450)
+// 2: function(-400)
+// ...
