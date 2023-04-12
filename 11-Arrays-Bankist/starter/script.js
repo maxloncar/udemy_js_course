1afad99c6ce9362f08aef03eaafcde4d417cf8cc
Max Loncar <max.loncar@devot.team>
2023-04-12 10:19:46 +0200

    Section 11: The Magic of Chaining Methods - finish

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -92,14 +92,22 @@ const calcDisplaySummary = function (movements) {
   const incomes = movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-
   labelSumIn.textContent = `${incomes}€`;
 
   const outcomes = movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-
   labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int > 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 calcDisplaySummary(account1.movements);
 
