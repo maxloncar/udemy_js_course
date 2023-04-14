91f36b535640abb1d3e15202844e42f801e9262e
Max Loncar <max.loncar@devot.team>
2023-04-14 14:11:26 +0200

    Section 12: The Remainder Operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -289,7 +289,7 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(17));
 console.log(Number.isInteger(17.0));
 console.log(Number.isInteger(17 / 0));
-*/
+
 
 // MATH AND ROUNDING
 console.log(Math.sqrt(25));
@@ -332,3 +332,31 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+*/
+
+// THE REMAINDER OPERATOR (% ostatak djeljenja)
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
