5e618f7440d07363806e5c86186268b06927183b
Max Loncar <max.loncar@devot.team>
2023-04-19 10:21:26 +0200

    Section 13: Event Delegation - Implementing Page Navigation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -27,6 +29,69 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+// IMPLEMENTING SMOOTH SCROLLING
+// Button scrolling
+btnScrollTo.addEventListener('click', function () {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log(
+  //   'Current scroll (X/Y) - deprecated',
+  //   window.pageXOffset,
+  //   window.pageYOffset
+  // );
+  // console.log('Current scroll (X/Y)', window.scrollX, window.scrollY);
+
+  // clientWidth and clientHeight - not countinh scrollbars, only viewport dimensions
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  // Scrolling
+  // pageXOffset and pageYOffset are deprecated
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// EVENT DELEGATION: IMPLEMENTING PAGE NAVIGATION
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(el => {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
@@ -117,49 +182,8 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 logo.className = 'max';
-*/
 
-// IMPLEMENTING SMOOTH SCROLLING
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function (e) {
-  // const s1coords = section1.getBoundingClientRect();
-  // console.log(s1coords);
-
-  // console.log(e.target.getBoundingClientRect());
-
-  // console.log(
-  //   'Current scroll (X/Y) - deprecated',
-  //   window.pageXOffset,
-  //   window.pageYOffset
-  // );
-  // console.log('Current scroll (X/Y)', window.scrollX, window.scrollY);
-
-  // clientWidth and clientHeight - not countinh scrollbars, only viewport dimensions
-  // console.log(
-  //   'height/width viewport',
-  //   document.documentElement.clientHeight,
-  //   document.documentElement.clientWidth
-  // );
-
-  // Scrolling
-  // pageXOffset and pageYOffset are deprecated
-  // window.scrollTo(
-  //   s1coords.left + window.scrollX,
-  //   s1coords.top + window.scrollY
-  // );
-
-  // window.scrollTo({
-  //   left: s1coords.left + window.scrollX,
-  //   top: s1coords.top + window.scrollY,
-  //   behavior: 'smooth',
-  // });
-
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-
-/*
 // TYPES OF EVENTS AND EVENT HANDLERS
 const h1 = document.querySelector('h1');
 
@@ -175,7 +199,7 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // h1.onmouseenter = function (e) {
 //   alert('addEventListener: Great! You are reading the heading :D');
 // };
-*/
+
 
 // EVENT PROPAGATION IN PRACTICE
 // rgb(255, 255, 255)
@@ -203,3 +227,4 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('NAV', e.target, e.currentTarget);
 });
+*/
