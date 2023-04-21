e301e57bb6254570c7b68c0f4d2333cf089097fc
Max Loncar <max.loncar@devot.team>
2023-04-21 13:18:33 +0200

    Section 14: Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// CONSTRUCTOR FUNCTIONS AND THE NEW OPERATOR
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2023 - this.birthYear);
+  // };
+};
+
+const max = new Person('Max', 1998);
+console.log(max);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1995);
+console.log(matilda, jack);
+
+console.log(max instanceof Person);
