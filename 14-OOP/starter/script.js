6b3f69f2dbe9e8da96f16d8747575a5975064e4f
Max Loncar <max.loncar@devot.team>
2023-04-25 12:16:39 +0200

    Section 14: Coding Challenge #3

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -262,7 +262,7 @@ console.log(ford);
 
 ford.brake();
 ford.accelerate();
-*/
+
 
 // INHERITANCE BETWEEN "CLASSES": CONSTRUCTOR FUNCTIONS
 const Person = function (firstName, birthYear) {
@@ -301,3 +301,47 @@ console.log(mike instanceof Object); // true
 
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+// CODING CHALLENGE #3
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed; // current speed in km/h
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
