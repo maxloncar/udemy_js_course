b25a5e11db9f4a68dec67da937c7db8979d0259f
Max Loncar <max.loncar@devot.team>
2023-04-26 10:58:48 +0200

    Section 14: Another Class Example

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -407,7 +407,7 @@ class StudentCl extends PersonCl {
 const pero = new StudentCl('Pero Perić', 2000, 'Computer Science');
 pero.introduce();
 pero.calcAge();
-*/
+
 
 // INHERITANCE BETWEEN "CLASSES": OBJECT.CREATE
 const PersonProto = {
@@ -439,3 +439,49 @@ const jure = Object.create(StudentProto);
 jure.init('Jure', 1996, 'Computer Science');
 jure.introduce();
 jure.calcAge();
+*/
+
+// ANOTHER CLASS EXAMPLE
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Max', 'EUR', 1111);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
