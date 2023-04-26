23c35d8c2c108099b9fd6f2819cc434c41e45a41
Max Loncar <max.loncar@devot.team>
2023-04-26 13:36:28 +0200

    Section 14: Encapsulation - Private Class Fields and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -441,53 +441,79 @@ jure.introduce();
 jure.calcAge();
 */
 
-// ANOTHER CLASS EXAMPLE & ENCAPSULATION: PROTECTED PROPERTIES AND METHODS
+// ANOTHER CLASS EXAMPLE, ENCAPSULATION: PROTECTED PROPERTIES AND METHODS & ENCAPSULATION: PRIVATE CLASS FIELDS AND METHODS
+
+// 1) Public fields
+// 2) Private fields
+// 3) Public methods
+// 4) Private methods
+// (there is also the static version)
+
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
     // protected property
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  // 3) Public methods
   // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  _approveLoan(val) {
-    return true;
-  }
-
   requestLoan(val) {
+    // if (this.#approveLoan(val)) {
     if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
   }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
 }
 
 const acc1 = new Account('Max', 'EUR', 1111);
 
-// acc1._movements.push(250);
-// acc1._movements.push(-140);
+// acc1.#movements.push(250);
+// acc1.#movements.push(-140);
+// acc1._approveLoan(1000);
+
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1._approveLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
-console.log(acc1._pin);
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+Account.helper();
