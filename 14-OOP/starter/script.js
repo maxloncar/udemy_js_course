460b219ac29d5c5850625aa5acb1cfe66afe8204
Max Loncar <max.loncar@devot.team>
2023-04-26 11:54:30 +0200

    Section 14: Encapsulation - Protected Properties and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -441,33 +441,38 @@ jure.introduce();
 jure.calcAge();
 */
 
-// ANOTHER CLASS EXAMPLE
+// ANOTHER CLASS EXAMPLE & ENCAPSULATION: PROTECTED PROPERTIES AND METHODS
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin;
+    // protected property
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
   // Public interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -476,12 +481,13 @@ class Account {
 
 const acc1 = new Account('Max', 'EUR', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
-console.log(acc1.pin);
+console.log(acc1._pin);
